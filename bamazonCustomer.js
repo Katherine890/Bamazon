5d840962be8c020a171d8f2e88cb8ee8cc537157
KitKat <katherine_nguyen7@yahoo.com>
2018-12-11 23:37:58 -0800

    updated inputs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,43 +14,95 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
+    start();
 });
 
-function afterConnection() {
+function start() {
     connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
         if (err) throw err;
         console.log(JSON.stringify(res, null, 2));
-        //connection.end();
-        runSearch();
-    });
-}
-
-function runSearch() {
-    inquirer
-        .prompt([
-            {
+        inquirer
+            .prompt({
+                name: "productId",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    } else {
+                        return false,
+                            console.log(" \n PLEASE ENTER A VALID ID NUMBER");
+                    }
+                },
                 type: "input",
+                // choices: function (value) {
+                //   var choiceArray = [];
+                // for (var i = 0; i < res.length; i++) {
+                //   choiceArray.push(res[i].item_id);
+                //}
+                //return choiceArray;
+                //},
                 message: "WHAT IS THE ID OF THE PRODUCT YOU WOULD LIKE TO BUY?",
-                name: "productId"
-            },
-            
-            {
-                type: "input",
-                message: "HOW MANY UNITS OF THIS PRODUCT WOULD YOU LIKE TO BUY?",
-                name: "username"
-            }
-        ])
-        .then(function (answer) {
-            determineAnswer(answer);
-        });
-}
+
+
+            }).then(function (answer) {
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].item_id == answer.productId) {
+                        var chosenItem = res[i];
+                        inquirer
+                            .prompt({
+
+                                name: "chosen_Quantity",
+                                type: "input",
+                                message: "HOW MANY UNITS OF THIS PRODUCT WOULD YOU LIKE TO BUY?",
+                                validate: function (value) {
+                                    if (isNaN(value) == false) {
+                                        return true;
+                                    } else {
+                                        return false,
+                                            console.log(" \n PLEASE ENTER A NUMBER");
+                                    }
+                                }
+                            })
+                            .then(function (answer) {
+
+                                if (parseInt(answer.chosen_Quantity) < chosenItem.stock_quantity) {
+                                    // There is enough quantity, update the db.
+                                    connection.query(
+                                        "SELECT * FROM products",
+                                        "UPDATE auctions SET ? WHERE ?",
+                                        [
+                                            {
+                                                stock_quantity: answer.chosen_Quantity
+                                            },
+                                            {
+                                                item_id: chosenItem.item_id
+                                            }
+                                        ],
+                                        function (error) {
+                                            if (error) throw err;
+                                            console.log("PURCHASE SUCCESSFUL!");
+                                            //  start();
+                                        }
+                                    );
+                                }
+                                else {
+                                    // Not enough products in stock
+                                    console.log("INSUFFICIENT QUANTITY!");
+                                    //start();
+                                }
+
+                            })
+                    }
+
+
+                }
+            })
 
 
 
-// connect to mysql server and sql database
-//connection.connect(function(err) {
-  //  if (err) throw err;
-    //run start function after connection is made to prompt the user
-   // start();
-//});
+
+    })
+
+
+};
+
+
